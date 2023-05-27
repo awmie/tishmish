9832585fbc1d2b1a0f6d9d12f798a8b1b6bcc5e2
awmie <amritavakole@gmail.com>
2023-05-27 11:35:42 +0530

    changes

--- app.py
+++ app.py
@@ -141,36 +141,6 @@ async def on_nextwave_track_end(player: nextwave.Player, track: nextwave.Track, 
             )
         )
         
-@bot.event
-async def on_voice_state_update(member, before, after):
-    # Check if the bot's voice state has changed
-    if member.id == bot.user.id and before.channel != after.channel and (before.channel is not None and after.channel is None):
-        voice_client = nextcord.utils.get(bot.voice_clients, guild=before.channel.guild)
-        if voice_client is not None:
-            # Get the player associated with the voice client
-            if voice_client.guild is not None:
-                player = get_player(voice_client.guild.id)
-                if player is not None:
-                    # Clear the queue for the player
-                    player.queue.clear()
-
-            # Disconnect the voice client
-            await voice_client.disconnect()
-
-    # Auto-disconnect if all participants leave the voice channel
-    if before.channel is not None and bot.user in before.channel.members and len(before.channel.members) == 1:
-        for vc in bot.voice_clients:
-            if vc.channel == before.channel:
-                # Get the player associated with the voice client
-                if vc.guild is not None:
-                    player = get_player(vc.guild.id)
-                    if player is not None:
-                        # Clear the queue for the player
-                        player.queue.clear()
-
-                # Disconnect the voice client
-                await vc.disconnect(force=True)
-                break
 
 @bot.event
 async def on_command_error(ctx: commands.Context, error):
